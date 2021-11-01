43de962b565afa5300e6fa87ee4350cac9f52b2b
wenxiang <1612780426@qq.com>
2021-11-01 14:29:50 +0800

    fix:优化ajax请求

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -8,16 +8,24 @@ export default function ajax(url, data = {}, type = 'GET') {
     //拼请求参数串
     //data:{username:tom,password:123}
     //paramStr:username=tom&password=123
-    let paramStr = ''
-    Object.keys(data).forEach(key => {
-        paramStr += key + '=' + data[key] + '&'
+    return new Promise((resolve, reject) => {
+        let promise
+        let paramStr = ''
+        Object.keys(data).forEach(key => {
+            paramStr += key + '=' + data[key] + '&'
+        })
+        if (paramStr) {
+            paramStr = paramStr.substring(0, paramStr.length - 1)
+        }
+        if (type === 'GET') {//发送GEI请求
+            promise = axios.get(url + '?' + paramStr)
+        } else {//否则发送POST请求
+            promise = axios.post(url, data)
+        }
+        promise.then((response) => {
+            resolve(response.data)
+        }).catch(error => {
+            reject(error)
+        })
     })
-    if (paramStr) {
-        paramStr = paramStr.substring(0, paramStr.length - 1)
-    }
-    if (type === 'GET') {//发送GEI请求
-        return axios.get(url + '?' +paramStr)
-    } else {//否则发送POST请求
-        return axios.post(url, data)
-    }
 }
